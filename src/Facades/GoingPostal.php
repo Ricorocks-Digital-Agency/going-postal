982d92f272af8fae4cdce1181749bda5536573c1
sam <sam@ricorocks.agency>
2021-05-07 14:25:54 +0100

    Updates Facade

--- src/Facades/GoingPostal.php
+++ src/Facades/GoingPostal.php
@@ -6,14 +6,14 @@ namespace RicorocksDigitalAgency\GoingPostal\Facades;
 
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Facade;
-use RicorocksDigitalAgency\GoingPostal\Address;
+use Illuminate\Contracts\Support\Arrayable;
 
 /**
  * Class GoingPostal
  * @package RicorocksDigitalAgency\GoingPostal\Facades
  *
  * @method static Collection lookup($postcode) Return addresses for the given postcode
- * @method static Address addressFor($identifier) Retrieve a single address from an identifier
+ * @method static Arrayable addressFor($identifier) Retrieve a single address from an identifier
  */
 class GoingPostal extends Facade
 {
